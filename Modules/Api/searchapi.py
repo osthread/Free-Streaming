a7fedc46d42b99078b809f69e22b248b368850ef
Maxie <76827213+MaxieDev@users.noreply.github.com>
2024-01-17 23:04:42 -0600

    Filter Dead Movies / Tv Shows

--- Modules/Api/searchapi.py
+++ Modules/Api/searchapi.py
@@ -1,56 +1,60 @@
-import requests 
+from requests import get 
 
 class Search:
     def __init__(self):
-        self.headers = {"accept": "application/json", "Authorization": self.get_token()}
+        self.db_manager = DatabaseManager()
+        self.headers = {"accept": "application/json", "Authorization": "API-Key"}
+ 
+    def check_media(self, results):
+        invalid_movies_ids = set()
+        for result in results:
+            response = get(f"https://vidsrc.to/embed/{result['media_type']}/{result['id']}")
+            if response.status_code == 404:
+                invalid_movies_ids.add(result["id"])
 
-    def get_token(self):
-        tokens = ["Api-Keys"]
+        return invalid_movies_ids
 
-        for t in tokens:
-            return t
-
-    def get_Upcoming(self):
-        url = "https://api.themoviedb.org/3/movie/upcoming?language=en-US"
-        response = requests.get(url, headers=self.headers)
-        upcoming = response.json().get('results', [])
-        return upcoming
-
-    def get_trending_movies(self):
-        url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
-        response = requests.get(url, headers=self.headers)
-        trending_movies = response.json().get('results', [])
-        return trending_movies
-
-    def get_trending_tv(self):
-        url = "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1"
-        response = requests.get(url, headers=self.headers)
-        trending_tv = response.json().get('results', [])
-        return trending_tv
-
-    def search_results(self, query):
-        url = f"https://api.themoviedb.org/3/search/multi?query={query}"
-
-        response = requests.get(url, headers=self.headers)
-
-        data = response.json()
-
-        original_results = data.get('results', [])
-
+    def modified_results(self, original_results, invalid_movies_ids):
+        required_keys = ['id', 'title', 'overview', 'poster_path', 'media_type','popularity', 'release_date']
         modified_and_valid_results = []
 
-        required_keys = [
-            'id', 'title', 'overview', 'poster_path', 'media_type',
-            'popularity', 'release_date']
-
         for item in original_results:
+            if item['id'] in invalid_movies_ids:
+                continue
+
             if 'name' in item:
                 item['title'] = item.pop('name')
-
+    
             if 'first_air_date' in item:
                 item['release_date'] = item.pop('first_air_date')
-
+    
             if all(key in item and item[key] for key in required_keys):
                 modified_and_valid_results.append(item)
+    
+        return modified_and_valid_results
 
+    def get_upcoming(self):
+        response = get("https://api.themoviedb.org/3/movie/upcoming?language=en-US", headers=self.headers)
+        upcoming_movies = response.json().get('results', [])
+        return upcoming_movies
+
+    def get_trending_movies(self): 
+        response = get("https://api.themoviedb.org/3/trending/movie/day?language=en-US", headers=self.headers)
+        original_results = response.json().get('results', [])
+        invalid_movies_ids = self.check_media(original_results)
+        modified_and_valid_results = self.modified_results(original_results, invalid_movies_ids)
+        return modified_and_valid_results
+
+    def get_trending_tv(self):
+        response = get("https://api.themoviedb.org/3/trending/tv/day?language=en-US", headers=self.headers)
+        original_results = response.json().get('results', [])
+        invalid_movies_ids = self.check_media(original_results)
+        modified_and_valid_results = self.modified_results(original_results, invalid_movies_ids)
+        return modified_and_valid_results
+
+    def search_results(self, query):
+        response = get(f"https://api.themoviedb.org/3/search/multi?query={query}", headers=self.headers)
+        original_results = response.json().get('results', [])
+        invalid_movies_ids = self.check_media(original_results)
+        modified_and_valid_results = self.modified_results(original_results, invalid_movies_ids)
         return modified_and_valid_results
