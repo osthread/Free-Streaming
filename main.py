a1f898ca9897d46262a78ed9183a386c1b0f828b
Maxie <76827213+MaxieDev@users.noreply.github.com>
2024-01-18 09:50:39 -0600

    Code Optimizations

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ class Streaming:
         self.sapi = Search()
         self.headers = {"accept": "application/json", "Authorization": "API-Key"}
 
+    def check_movie(self, media_type, video_id):
+        response = requests.get(f"https://api.themoviedb.org/3/{media_type}/{video_id}", headers=self.headers).json()
+
+        overview = response.get("overview")
+        title = response.get("original_title" if media_type == "movie" else "name")
+        thumbnail = response.get("poster_path")
+
+        video_url = f"https://vidsrc.to/embed/{media_type}/{video_id}"
+        check_res = requests.get(video_url)
+        if check_res.status_code != 200:
+            video_url = video_url.replace("vidsrc.to", "vidsrc.xyz")
+
+        return video_url, overview, title, thumbnail
+    
     def routes(self):
         @self.app.route('/watch/<key>')
         def index(key):
@@ -23,23 +37,15 @@ class Streaming:
             query = request.args.get('query')
             search = self.sapi.search_results(query)
             return render_template('results.html', search=search)
-
+            
         @self.app.route('/watch/<media_type>/<video_id>')
         def watch_video(media_type, video_id):
             if media_type not in ["movie", "tv"]: 
                 abort(404)
 
-            response = requests.get(f"https://api.themoviedb.org/3/{media_type}/{video_id}", headers=self.headers).json()
-            overview = response.get("overview")
-            title = response.get("original_title" if media_type == "movie" else "name")
-            thumbnail = response.get("poster_path")
+            response = self.check_movie(media_type, video_id)
 
-            video_url = f"https://vidsrc.to/embed/{media_type}/{video_id}"
-            check_res = requests.get(video_url)
-            if check_res.status_code != 200:
-                video_url = video_url.replace("vidsrc.to", "vidsrc.xyz")
-
-            return render_template('watch.html', video_url=video_url, title=title, overview=overview, thumbnail=thumbnail)
+            return render_template('watch.html', video_url=response[0], title=response[2], overview=response[1], thumbnail=response[3])
 
     def run(self):
         CORS(self.app)
