1398a5b8de008b4fda92100fa4699818b87ec509
Maxie <76827213+MaxieDev@users.noreply.github.com>
2024-01-17 23:02:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 from waitress import serve
 from flask_cors import CORS
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template, abort
 
 from Modules.Api.searchapi import Search
 
-import os, string, requests
+import os, string, requests, sys
 
 class Streaming:
     def __init__(self):
         self.app = Flask(__name__)
+        self.db_manager = DatabaseManager()
         self.routes()
         self.sapi = Search()
-        self.headers = {"accept": "application/json", "Authorization": self.get_token()}
-
-    def get_token(self):
-        tokens = ["Api_Keys"]
-
-        for t in tokens:
-            return t
+        self.headers = {"accept": "application/json", "Authorization": "API-Key"}
 
     def routes(self):
-        @self.app.route('/')
-        def index():
-            return render_template('index.html', movies=self.sapi.get_trending_movies(), tv=self.sapi.get_trending_tv(), upcoming=self.sapi.get_Upcoming())
+        @self.app.route('/watch/<key>')
+        def index(key):
+            return render_template('index.html', movies=self.sapi.get_trending_movies(), tv=self.sapi.get_trending_tv(), upcoming=self.sapi.get_upcoming())
 
-        @self.app.route('/search')
+        @self.app.route('/watch/search')
         def searchresults():
             query = request.args.get('query')
             search = self.sapi.search_results(query)
@@ -32,33 +27,27 @@ class Streaming:
 
         @self.app.route('/watch/<media_type>/<video_id>')
         def watch_video(media_type, video_id):
+            if media_type not in ["movie", "tv"]: 
+                abort(404)
+
             response = requests.get(f"https://api.themoviedb.org/3/{media_type}/{video_id}", headers=self.headers).json()
-            overview = response.get("overview")
-            if media_type == "movie":
-                video_url = f"https://vidsrc.to/embed/movie/{video_id}"
-                title = response.get("original_title")
-                thumbnail = response.get("poster_path")
+            if response.status_code != 200:
+                abort(response.status_code)
 
-            elif media_type == "tv":
-                video_url = f"https://vidsrc.to/embed/tv/{video_id}"
-                title = response.get("name")
-                thumbnail = response.get("poster_path")
-            
-            return render_template('watch.html', video_url=video_url, title=title, overview=overview, thumbnail=thumbnail) 
-
-        @self.app.route('/watch/anime/<video_id>')
-        def watch_anime(video_id):
-            response = requests.get(f"https://api.themoviedb.org/3/tv/{video_id}", headers=self.headers).json()
             overview = response.get("overview")
-            video_url = f"https://vidsrc.me/embed/tv?tmdb={video_id}"
-            title = response.get("name")
+            title = response.get("original_title" if media_type == "movie" else "name")
             thumbnail = response.get("poster_path")
-            
-            return render_template('watch.html', video_url=video_url, title=title, overview=overview, thumbnail=thumbnail) 
+
+            video_url = f"https://vidsrc.to/embed/{media_type}/{video_id}"
+            check_res = requests.get(video_url)
+            if check_res.status_code != 200:
+                video_url = video_url.replace("vidsrc.to", "vidsrc.xyz")
+
+            return render_template('watch.html', video_url=video_url, title=title, overview=overview, thumbnail=thumbnail)
 
     def run(self):
         CORS(self.app)
-        serve(self.app, host="0.0.0.0", port=9999)
+        serve(self.app, host="127.0.0.1", port=9999)
 
 if __name__ == '__main__':
     Trinix = Streaming()
